91268c847c4534022cf55011e9bf38b43befc1a1
Andriikryl <andreyk557@gmail.com>
2024-02-10 22:03:55 +0200

    add createGrid function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,27 @@
   import { emoji } from "./emoji";
   type State = "start" | "playing" | "paused" | "won" | "lost";
   let state: State = "start";
+  let size = 20;
+  let grid = createGrid();
+  function createGrid() {
+    // only want unique cards
+    let cards = new Set<string>();
+    // half because we duplicate the cards
+    let maxSize = size / 2;
+
+    while (cards.size < maxSize) {
+      // pick random emoji
+      const randomIndex = Math.floor(Math.random() * emoji.length);
+      cards.add(emoji[randomIndex]);
+    }
+
+    // duplicate and shuffle cards
+    return shuffle([...cards, ...cards]);
+  }
+
+  function shuffle<Items>(array: Items[]) {
+    return array.sort(() => Math.random() - 0.5);
+  }
 </script>
 
 <div>das</div>
